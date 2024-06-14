4aa89b31409fb2ca311ebb6aa9db8ce61a4f4d18
gameenudara <gameenudara567@gmail.com>
2024-06-14 07:24:30 +0530

    project initial

---
+++ GymBuddies_api/src/main/java/lk/GymBuddies_api/GymBuddiesApiApplication.java
@@ -0,0 +1,13 @@
+package lk.GymBuddies_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymBuddiesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymBuddiesApiApplication.class, args);
+	}
+
+}
---
+++ GymBuddies_api/src/test/java/lk/GymBuddies_api/GymBuddiesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.GymBuddies_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymBuddiesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
